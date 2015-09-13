7365042f0239c66b49ec2dd0e4c30e9ad90828ce
Michael Nichols <mrnichol@garlandisd.net>
2015-09-12 19:06:44 -0500

    Added world class.

---
+++ SpaceWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class SpaceWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SpaceWorld extends World
+{
+
+    /**
+     * Constructor for objects of class SpaceWorld.
+     * 
+     */
+    public SpaceWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 600, 1); 
+    }
+}
